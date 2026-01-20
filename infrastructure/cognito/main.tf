35361fe728ea6cc143b20c045171456a01ba7075
gauciv <vincentaugusto16@gmail.com>
2026-01-20 17:41:53 +0800

    refactor(infra): consolidate cleanup lambda into cognito directory

--- infrastructure/cognito/main.tf
+++ infrastructure/cognito/main.tf
@@ -187,3 +187,90 @@ resource "aws_cognito_user_pool_client" "main" {
   prevent_user_existence_errors = "ENABLED"
   generate_secret               = false
 }
+
+# Cleanup Lambda for unconfirmed users
+data "archive_file" "cleanup_lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../lambda/cognito-cleanup"
+  output_path = "${path.module}/cognito-cleanup.zip"
+}
+
+resource "aws_iam_role" "cleanup_lambda_role" {
+  name = "trustate-cognito-cleanup-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "cleanup_lambda_policy" {
+  name = "trustate-cognito-cleanup-policy"
+  role = aws_iam_role.cleanup_lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "cognito-idp:ListUsers",
+          "cognito-idp:AdminDeleteUser"
+        ]
+        Resource = aws_cognito_user_pool.main.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "cognito_cleanup" {
+  filename         = data.archive_file.cleanup_lambda_zip.output_path
+  function_name    = "trustate-cognito-cleanup"
+  role            = aws_iam_role.cleanup_lambda_role.arn
+  handler         = "index.handler"
+  source_code_hash = data.archive_file.cleanup_lambda_zip.output_base64sha256
+  runtime         = "nodejs20.x"
+  timeout         = 60
+
+  environment {
+    variables = {
+      COGNITO_USER_POOL_ID = aws_cognito_user_pool.main.id
+      AWS_REGION          = var.aws_region
+    }
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "daily_cleanup" {
+  name                = "trustate-cognito-cleanup-daily"
+  description         = "Trigger Cognito cleanup daily"
+  schedule_expression = "rate(1 day)"
+}
+
+resource "aws_cloudwatch_event_target" "cleanup_lambda_target" {
+  rule      = aws_cloudwatch_event_rule.daily_cleanup.name
+  target_id = "CognitoCleanupLambda"
+  arn       = aws_lambda_function.cognito_cleanup.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.cognito_cleanup.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.daily_cleanup.arn
+}
