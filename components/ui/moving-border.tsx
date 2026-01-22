0501ed5e75faef336cacd76ad5a4cd1974f2d549
gauciv <vincentaugusto16@gmail.com>
2026-01-22 22:07:34 +0800

    fix(ui): resolve buttonprops type import in moving-border

--- components/ui/moving-border.tsx
+++ components/ui/moving-border.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { Button as BaseButton, ButtonProps } from "@/components/ui/button";
+import { Button as BaseButton } from "@/components/ui/button";
+import { ComponentProps } from "react";
+
+type ButtonProps = ComponentProps<typeof BaseButton>;
 
 export function Button({ children, ...props }: ButtonProps & { borderRadius?: string; containerClassName?: string; borderClassName?: string }) {
   return <BaseButton {...props}>{children}</BaseButton>;
